0cfc1d42f06b279bc135e088caa225de98758e0b
SOJO <sojoocook@naver.com>
2020-06-25 12:23:44 +0900

    ServiceDTO

---
+++ src/main/java/kh/pet/dto/ServiceDTO.java
@@ -0,0 +1,23 @@
+package kh.pet.dto;
+
+public class ServiceDTO {
+	private String service;
+
+	public ServiceDTO(String service) {
+		super();
+		this.service = service;
+	}
+
+	public ServiceDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getService() {
+		return service;
+	}
+
+	public void setService(String service) {
+		this.service = service;
+	}
+}
